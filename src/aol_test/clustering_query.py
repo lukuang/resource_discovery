eb50e7d7d8e47a2b43248be2ec6a37a2e3dcd85e
lukuang <lukuang1989@gmail.com>
2016-02-12 11:06:27 -0500

    aol - first commit

---
+++ src/aol_test/clustering_query.py
@@ -0,0 +1,18 @@
+"""
+clustering queries based on the domain
+"""
+
+import os
+import json
+import sys
+import re
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("")
+    args=parser.parse_args()
+
+if __name__=="__main__":
+    main()
+
